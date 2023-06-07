0af28f5c4a34152a6f0adfa3cb83ad362af1bea0
Sujan Chandra Roy <sujan.007.ice@gmail.com>
2023-06-07 19:01:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 import cv2
 import argparse
 import numpy as np
+
+argparser = argparse.ArgumentParser(description='Simple implementation of Yolov3 algorithm in Python, using custom Dataset.')
+
+argparser.add_argument('--img', type=str)
+argparser.add_argument('--video', type=str)
+argparser.add_argument('--out', type=str)
