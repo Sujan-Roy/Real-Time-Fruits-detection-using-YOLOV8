25b58fd08596611e682176f30473102846af3c3b
Sujan Chandra Roy <sujan.007.ice@gmail.com>
2023-06-14 21:51:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ confidence, threshold = 0.5, 0.3
 labelPath = './obj.names'
 labels = open(labelPath).read().strip().split('\n')
 
+
 weightsPath = './yolov3-tiny.weights'
 configPath = './yolov3-tiny.cfg'
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
 
 layer_names = net.getLayerNames()
 yolo_layers = []
